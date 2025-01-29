6f680112cb34cb82d2a32f5aeb30b7e857b55a83
dread557 <dadaolawunmi09@gmail.com>
2025-01-29 10:19:59 +0100

    feat(onboarding): complete onboarding

--- src/app/(dashboard)/_components/SideModalWrapper.tsx
+++ src/app/(dashboard)/_components/SideModalWrapper.tsx
@@ -10,8 +10,8 @@ interface Props {
 const SideModalWrapper = ({ children, close }: Props) => {
   return (
     <Overlay close={close}>
-      <div className="h-screen p-[30px] bg-raiz-gray-50 rounded-tl-[36px] rounded-bl-[36px] justify-start items-center gap-2 inline-flex fixed right-0 top-0 bottom-0 w-[28.57%]">
-        {children}
+      <div className="h-screen p-[30px] bg-raiz-gray-50 rounded-tl-[36px] rounded-bl-[36px] justify-start  gap-2 inline-flex fixed right-0 top-0 bottom-0 w-[28.57%]">
+        <div className="w-full">{children}</div>
       </div>
     </Overlay>
   );
