6125679acd849da7feb4e335b99484ba4d9d445a
dread557 <dadaolawunmi09@gmail.com>
2025-02-04 05:57:36 +0100

    feat (dashbooard) : implement empty states

--- src/app/(dashboard)/_components/Transactions.tsx
+++ src/app/(dashboard)/_components/Transactions.tsx
@@ -1,63 +1,72 @@
 import React from "react";
 import Image from "next/image";
 
-const sampleTrx = [
-  {
-    id: 1,
-    requester: "Desirae Bergson",
-    time: "18 Mar 2023 @ 4:42 PM",
-    amount: 4000000,
-    imageUrl: "/images/pfp.png",
-    type: "credit",
-  },
-  {
-    id: 2,
-    requester: "Sarah K",
-    time: "8 Apr 2023 @ 2:20 PM",
-    amount: 2500000,
-    imageUrl: "/images/pfp.png",
-    type: "credit",
-  },
-  {
-    id: 3,
-    requester: "Michael D",
-    time: "8 Apr 2023 @ 2:20 PM",
-    amount: 1500000,
-    imageUrl: "/images/pfp.png",
-    type: "debit",
-  },
-  {
-    id: 4,
-    requester: "Linda W",
-    time: "8 Apr 2023 @ 2:20 PM",
-    amount: 3200000,
-    imageUrl: "/images/pfp.png",
-    type: "credit",
-  },
-  {
-    id: 5,
-    requester: "David T",
-    time: "18 Mar 2023 @ 4:42 PM",
-    amount: 500000,
-    imageUrl: "/images/pfp.png",
-    type: "debit",
-  },
-  {
-    id: 6,
-    requester: "Jessica L",
-    time: "18 Mar 2023 @ 4:42 PM",
-    amount: 4500000,
-    imageUrl: "/images/pfp.png",
-    type: "credit",
-  },
-  {
-    id: 7,
-    requester: "Kevin M",
-    time: "18 Mar 2023 @ 4:42 PM",
-    amount: 1200000,
-    imageUrl: "/images/pfp.png",
-    type: "credit",
-  },
+type Transaction = {
+  id: number;
+  requester: string;
+  time: string;
+  amount: number;
+  imageUrl: string;
+  type: string;
+};
+
+const sampleTrx: Transaction[] = [
+  // {
+  //   id: 1,
+  //   requester: "Desirae Bergson",
+  //   time: "18 Mar 2023 @ 4:42 PM",
+  //   amount: 4000000,
+  //   imageUrl: "/images/pfp.png",
+  //   type: "credit",
+  // },
+  // {
+  //   id: 2,
+  //   requester: "Sarah K",
+  //   time: "8 Apr 2023 @ 2:20 PM",
+  //   amount: 2500000,
+  //   imageUrl: "/images/pfp.png",
+  //   type: "credit",
+  // },
+  // {
+  //   id: 3,
+  //   requester: "Michael D",
+  //   time: "8 Apr 2023 @ 2:20 PM",
+  //   amount: 1500000,
+  //   imageUrl: "/images/pfp.png",
+  //   type: "debit",
+  // },
+  // {
+  //   id: 4,
+  //   requester: "Linda W",
+  //   time: "8 Apr 2023 @ 2:20 PM",
+  //   amount: 3200000,
+  //   imageUrl: "/images/pfp.png",
+  //   type: "credit",
+  // },
+  // {
+  //   id: 5,
+  //   requester: "David T",
+  //   time: "18 Mar 2023 @ 4:42 PM",
+  //   amount: 500000,
+  //   imageUrl: "/images/pfp.png",
+  //   type: "debit",
+  // },
+  // {
+  //   id: 6,
+  //   requester: "Jessica L",
+  //   time: "18 Mar 2023 @ 4:42 PM",
+  //   amount: 4500000,
+  //   imageUrl: "/images/pfp.png",
+  //   type: "credit",
+  // },
+  // {
+  //   id: 7,
+  //   requester: "Kevin M",
+  //   time: "18 Mar 2023 @ 4:42 PM",
+  //   amount: 1200000,
+  //   imageUrl: "/images/pfp.png",
+  //   type: "credit",
+  // },
 ];
 
 const Transactions = () => {
@@ -80,32 +89,57 @@ const Transactions = () => {
         </button>
       </div>
       <div className="flex flex-col gap-3 w-full max-h-72 overflow-y-scroll">
-        {sampleTrx.map((each) => (
-          <div
-            key={each.id}
-            className="font-monzo flex items-center justify-between w-full"
-          >
-            <div className="flex gap-[14px]">
-              <Image
-                src={each.imageUrl}
-                width={48}
-                height={48}
-                alt="requester"
-              />
-              <div className="flex flex-col gap-1">
-                <p className=" text-raiz-gray-950 text-sm font-semibold ">
-                  {each.requester}
-                </p>
-                <p className="opacity-50 text-raiz-gray-950 text-xs font-normal  leading-[15px]">
-                  {each.time}
-                </p>
+        {sampleTrx?.length > 0 ? (
+          sampleTrx?.map((each, index) => (
+            <div
+              key={index}
+              className="font-monzo flex items-center justify-between w-full"
+            >
+              <div className="flex gap-[14px]">
+                <Image
+                  src={each.imageUrl}
+                  width={48}
+                  height={48}
+                  alt="requester"
+                />
+                <div className="flex flex-col gap-1">
+                  <p className=" text-raiz-gray-950 text-sm font-semibold ">
+                    {each.requester}
+                  </p>
+                  <p className="opacity-50 text-raiz-gray-950 text-xs font-normal  leading-[15px]">
+                    {each.time}
+                  </p>
+                </div>
               </div>
+              <span className="text-raiz-gray-900 text-sm font-semibold  leading-tight ">
+                ₦{each.amount.toLocaleString()}
+              </span>
             </div>
-            <span className="text-raiz-gray-900 text-sm font-semibold  leading-tight ">
-              ₦{each.amount.toLocaleString()}
-            </span>
+          ))
+        ) : (
+          <div className="flex items-center gap-[14px]">
+            <svg width="48" height="48" viewBox="0 0 48 48" fill="none">
+              <rect width="48" height="48" rx="24" fill="#EBEDF4" />
+              <path
+                d="M34 24C34 29.52 29.52 34 24 34C18.48 34 14 29.52 14 24C14 18.48 18.48 14 24 14C29.52 14 34 18.48 34 24Z"
+                stroke="#443852"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M27.7099 27.1798L24.6099 25.3298C24.0699 25.0098 23.6299 24.2398 23.6299 23.6098V19.5098"
+                stroke="#443852"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <p className=" text-raiz-gray-600 text-sm font-semibold font-monzoo">
+              No transactions yet
+            </p>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
