81b37d604127190584d6d0421f9a6bf3b4335fa9
dread557 <dadaolawunmi09@gmail.com>
2025-02-19 13:21:25 +0100

    feat(onboarding): finish ui

--- src/app/(auth)/login/_components/LoginForm.tsx
+++ src/app/(auth)/login/_components/LoginForm.tsx
@@ -1,10 +1,13 @@
 "use client";
+import Image from "next/image";
 import Button from "@/components/ui/Button";
 import InputLabel from "@/components/ui/InputLabel";
 import { useFormik } from "formik";
 import Link from "next/link";
 import React, { useState } from "react";
 import InputField from "@/components/ui/InputField";
+import { toFormikValidationSchema } from "zod-formik-adapter";
+import { loginSchema } from "../../register/_components/validation";
 
 const LoginForm = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -14,56 +17,60 @@ const LoginForm = () => {
       email: "",
       password: "",
     },
+    validationSchema: toFormikValidationSchema(loginSchema),
     onSubmit: (val) => console.log("Submit", val),
   });
   return (
-    <section className="py-4 px-3 xl:px-8 w-[50%] xl:w-[46%] h-full flex flex-col font-monzo justify-between">
-      <div>
-        <h1 className="text-raiz-gray-950 text-[23px] font-semibold  leading-10">
-          Welcome Back
-        </h1>
-        <h6 className="text-raiz-gray-600 text-sm  leading-snug">Log in</h6>
-        <form
-          onSubmit={formik.handleSubmit}
-          className="flex flex-col gap-5 mt-[44px]"
-        >
-          <InputField
-            placeholder="Enter your email address"
-            label="Email"
-            type="email"
-            {...formik.getFieldProps("email")}
-            status={formik.errors.email ? "error" : null}
-            errorMessage={formik.touched.email && formik.errors.email}
-          />
-          <div className="flex justify-between items-center">
-            <InputLabel content="Password" />
-            <Link
-              className="text-right text-raiz-purple-90 text-sm font-semibold  leading-[16.80px] hover:underline"
-              href={"/forgot-password"}
-            >
-              Forgot your password?
-            </Link>
-          </div>
+    <section className="py-4 px-3 xl:px-8 w-[50%] xl:w-[46%] h-full flex flex-col font-monzo justify-between gap-[60px]">
+      <Image src={"/icons/Logo.svg"} width={91.78} height={32} alt="Logo" />
+      <div className="flex flex-col h-full  justify-between">
+        <div>
+          <h1 className="text-raiz-gray-950 text-[23px] font-semibold  leading-10">
+            Welcome Back
+          </h1>
+          <h6 className="text-raiz-gray-600 text-sm  leading-snug">Log in</h6>
+          <form
+            onSubmit={formik.handleSubmit}
+            className="flex flex-col gap-5 mt-[44px]"
+          >
+            <InputField
+              placeholder="Enter your email address"
+              label="Email"
+              type="email"
+              {...formik.getFieldProps("email")}
+              status={formik.errors.email ? "error" : null}
+              errorMessage={formik.touched.email && formik.errors.email}
+            />
+            <div className="flex justify-between items-center">
+              <InputLabel content="Password" />
+              <Link
+                className="text-right text-raiz-purple-90 text-sm font-semibold  leading-[16.80px] hover:underline"
+                href={"/forgot-password"}
+              >
+                Forgot your password?
+              </Link>
+            </div>
 
-          <InputField
-            type={showPassword ? "text" : "password"}
-            icon={!showPassword ? "/icons/eye-hide.svg" : "/icons/eye.svg"}
-            onClick={() => setShowPassword(!showPassword)}
-            iconPosition="right"
-            status={formik.errors.password ? "error" : null}
-            {...formik.getFieldProps("password")}
-          />
-        </form>
-      </div>
-      <div>
-        <Button>Login</Button>
-        <p className="text-raiz-gray-800 text-[13px] leading-tight text-center mt-6">
-          Don&#39;t have an account?{" "}
-          <Link href={"/register"} className="leading-[18.20px] font-bold ">
-            {" "}
-            Sign Up
-          </Link>
-        </p>
+            <InputField
+              type={showPassword ? "text" : "password"}
+              icon={!showPassword ? "/icons/eye-hide.svg" : "/icons/eye.svg"}
+              onClick={() => setShowPassword(!showPassword)}
+              iconPosition="right"
+              status={formik.errors.password ? "error" : null}
+              {...formik.getFieldProps("password")}
+            />
+          </form>
+        </div>
+        <div>
+          <Button disabled={!formik.dirty || !formik.isValid}>Login</Button>
+          <p className="text-raiz-gray-800 text-[13px] leading-tight text-center mt-6">
+            Don&#39;t have an account?{" "}
+            <Link href={"/register"} className="leading-[18.20px] font-bold ">
+              {" "}
+              Sign Up
+            </Link>
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/types/misc.ts
+++ src/types/misc.ts
@@ -10,12 +10,14 @@ export interface ISidebarMenuItem {
 export interface IRegisterFormValues {
   email: string;
   password: string;
-  username: string;
-  phone_number: string;
-  country_id: string;
+  // username: string;
+  // phone_number: string;
+  // country_id: string;
   referral_code?: string;
   otp: string;
-  useCases?: number[];
+  // useCases?: number[];
+  firstName: string;
+  lastName: string;
 }
 
 export interface AccountSetupStep {
