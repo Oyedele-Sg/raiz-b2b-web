b3dc87cd68dd6722598b3bd9577971d6463fa2c6
dread557 <dadaolawunmi09@gmail.com>
2025-03-28 19:25:21 +0100

    fix: build

--- src/app/(dashboard)/_components/send/usd/toBanks/ToUsdBanks.tsx
+++ src/app/(dashboard)/_components/send/usd/toBanks/ToUsdBanks.tsx
@@ -1,36 +1,36 @@
-import React, { useState } from "react";
-import BankTypeModal from "./BankTypeModal";
+import React from "react";
+// import BankTypeModal from "./BankTypeModal";
 
-type ToUsdBanksStepsType = "type" | "add-beneficiary";
+// type ToUsdBanksStepsType = "type" | "add-beneficiary";
 
 interface Props {
   close: () => void;
 }
 
-const ToUsdBanks = ({ close }: Props) => {
-  const [step, setStep] = useState<ToUsdBanksStepsType>("type");
-  const [bankType, setBankType] = useState<"us" | "int">();
+const ToUsdBanks = ({}: Props) => {
+  // const [step, setStep] = useState<ToUsdBanksStepsType>("type");
+  // const [bankType, setBankType] = useState<"us" | "int">();
 
-  const displayStep = () => {
-    switch (step) {
-      case "type":
-        return (
-          <BankTypeModal
-            close={close}
-            bankType={bankType}
-            setBankType={setBankType}
-          />
-        );
-      // case "add-beneficiary":
-      //     return (
+  // const displayStep = () => {
+  //   switch (step) {
+  //     case "type":
+  //       return (
+  //         <BankTypeModal
+  //           close={close}
+  //           bankType={bankType}
+  //           setBankType={setBankType}
+  //         />
+  //       );
+  //     // case "add-beneficiary":
+  //     //     return (
 
-      //     )
-      default:
-        break;
-    }
-  };
+  //     //     )
+  //     default:
+  //       break;
+  //   }
+  // };
 
-  return <div>{displayStep()}</div>;
+  return <div>h</div>;
 };
 
 export default ToUsdBanks;
